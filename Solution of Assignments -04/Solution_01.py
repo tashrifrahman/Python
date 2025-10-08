c8ac80a0d46ee18aff6811110509c2992f64f005
Tashrif <tashrifrahman2003@gmail.com>
2025-10-09 01:24:07 +0600

    Function

---
+++ Solution of Assignments -04/Solution_01.py
@@ -0,0 +1,12 @@
+def factorial(n):
+      if n<0:
+            return "Invalid."
+      if n==0:
+            return 1
+      result = 1
+      for i in range(1, n+1):
+            result *= i
+      return result
+
+n = int(input("Enter a number : "))
+print("Factorial : ",factorial(n))
